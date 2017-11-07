3425929139df853218766ffb5bd473ba74eddb0b
Victor Martins <victor.martins@sage.com>
2017-11-07 07:39:55 +0000

    1-8

--- roman_numerals/20171107/roman_numerals_converter.rb
+++ roman_numerals/20171107/roman_numerals_converter.rb
@@ -10,6 +10,21 @@ class RomanNumeralsConverter
   private
 
   def roman_numeral_for(number)
-    ROMAN_NUMERALS.detect { |k, v| v == number}.first
+    result = ''
+    while number >= 5
+      result << 'V'
+      number -= 5
+    end
+    while number > 0
+      result << 'I'
+      number -= 1
+    end
+    result
+
+    # if number <=8
+    #   return 'V' + 'I'*number
+    # end
+
+    # ROMAN_NUMERALS.detect { |k, v| v == number }.first
   end
 end
--- roman_numerals/20171107/spec/roman_numerals_converter_spec.rb
+++ roman_numerals/20171107/spec/roman_numerals_converter_spec.rb
@@ -3,11 +3,11 @@ require_relative './../roman_numerals_converter'
 RSpec.describe RomanNumeralsConverter do
   context 'converting arabic to roman' do
     it { expect(subject.convert(1)).to eq('I') }
-    xit { expect(subject.convert(3)).to eq('III') }
+    it { expect(subject.convert(3)).to eq('III') }
     xit { expect(subject.convert(4)).to eq('IV') }
     it { expect(subject.convert(5)).to eq('V') }
-    xit { expect(subject.convert(6)).to eq('VI') }
-    xit { expect(subject.convert(8)).to eq('VIII') }
+    it { expect(subject.convert(6)).to eq('VI') }
+    it { expect(subject.convert(8)).to eq('VIII') }
     xit { expect(subject.convert(9)).to eq('IX') }
     xit { expect(subject.convert(10)).to eq('X') }
   end
