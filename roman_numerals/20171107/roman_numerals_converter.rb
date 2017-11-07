abd688623b81f1e6ede37f448d28672b05ba0b54
Victor Martins <victor.martins@sage.com>
2017-11-07 07:26:05 +0000

    I and V

--- roman_numerals/20171107/roman_numerals_converter.rb
+++ roman_numerals/20171107/roman_numerals_converter.rb
@@ -1,5 +1,15 @@
 class RomanNumeralsConverter
+  ROMAN_NUMERALS = {
+    'I' => 1,
+    'V' => 5
+  }
   def convert(number)
-    'I'
+    roman_numeral_for(number)
+  end
+
+  private
+
+  def roman_numeral_for(number)
+    ROMAN_NUMERALS.detect { |k, v| v == number}.first
   end
 end
