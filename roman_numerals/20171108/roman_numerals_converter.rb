623eab0abd35ed6fcbc47ceec826e07a9027bec6
Victor Martins <victor.martins@sage.com>
2017-11-08 07:57:09 +0000

    all done

--- roman_numerals/20171108/roman_numerals_converter.rb
+++ roman_numerals/20171108/roman_numerals_converter.rb
@@ -16,12 +16,48 @@ class RomanNumeralsConverter
   }
 
   def convert(number)
+    return arabic_to_roman(number) if number.is_a?(Integer)
+    roman_to_arabic(number)
+  end
+
+  private
+
+  def arabic_to_roman(number)
     return convert_arabic_to_roman(number) if number > 0
     return "-#{convert_arabic_to_roman(-number)}" if number < 0
     ''
   end
 
-  private
+  def roman_to_arabic(number)
+    if number =~ /-/
+      number = number.sub('-','')
+      -convert_roman_to_arabic(number)
+    else
+      convert_roman_to_arabic(number)
+    end
+
+  end
+
+  def convert_roman_to_arabic(number)
+    result = 0
+    number.chars.each.with_index do |char, index|
+      next_char      = number[index+1]
+      current_number = char_to_number(char)
+      next_number    = char_to_number(next_char)
+
+      if next_number > current_number
+        result -= current_number
+      else
+        result += current_number
+      end
+
+    end
+    result
+  end
+
+  def char_to_number(char)
+    ROMAN_NUMERALS[char] || 0
+  end
 
   def convert_arabic_to_roman(number)
     ROMAN_NUMERALS.each_with_object('') do |roman_arabic, result|
