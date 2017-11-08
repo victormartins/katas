2687ebd3d0ccbf85a0236fb7e401b295188cbbe5
Victor Martins <victor.martins@sage.com>
2017-11-08 07:21:56 +0000

    all roman numbers but the negative ones

---
+++ roman_numerals/20171108/roman_numerals_converter.rb
@@ -0,0 +1,28 @@
+class RomanNumeralsConverter
+  ROMAN_NUMERALS = {
+    'M'  => 1000,
+    'CM' => 900,
+    'D'  => 500,
+    'CD' => 400,
+    'C'  => 100,
+    'XC' => 90,
+    'L'  => 50,
+    'XL' => 40,
+    'X'  => 10,
+    'IX' => 9,
+    'V'  => 5,
+    'IV' => 4,
+    'I'  => 1
+  }
+
+  def convert(number)
+    result = ''
+    ROMAN_NUMERALS.each_pair do |roman,arabic|
+      while number >= arabic
+        result << roman
+        number -= arabic
+      end
+    end
+    result
+  end
+end
