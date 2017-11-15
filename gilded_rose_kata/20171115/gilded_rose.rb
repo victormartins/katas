30af21a92f8d9cf08c04cec46b9b34b3a6c516dd
Victor Martins <victor.martins@sage.com>
2017-11-15 08:44:07 +0000

    wip

--- gilded_rose_kata/20171115/gilded_rose.rb
+++ gilded_rose_kata/20171115/gilded_rose.rb
@@ -1,5 +1,18 @@
-def update_quality(items)
-  items.each do |item|
+class Item
+  def initialize(item_data)
+    @item = item_data
+  end
+
+  def update
+    puts ''
+    puts '-' * 50
+    puts "#{item}".center(50)
+    puts '-' * 50
+    puts ''
+
+    return update_aged_item if item[:name].downcase =~ /aged/
+    return update_sulfuras_item if item[:name].downcase =~ /sulfuras/
+    return update_backstage_item if item[:name].downcase =~ /backstage/
     if item[:name] != 'Aged Brie' && item[:name] != 'Backstage passes to a TAFKAL80ETC concert'
       if item[:quality] > 0
         if item[:name] != 'Sulfuras, Hand of Ragnaros'
@@ -23,9 +36,12 @@ def update_quality(items)
         end
       end
     end
+
     if item[:name] != 'Sulfuras, Hand of Ragnaros'
       item[:sell_in] -= 1
     end
+
+
     if item[:sell_in] < 0
       if item[:name] != "Aged Brie"
         if item[:name] != 'Backstage passes to a TAFKAL80ETC concert'
@@ -44,19 +60,39 @@ def update_quality(items)
       end
     end
   end
+
+  private
+
+  attr_reader :item
+
+  def update_aged_item
+    item[:sell_in] -= 1
+    update_quality
+    update_quality if item[:sell_in] < 0
+  end
+
+  def update_backstage_item
+    update_quality
+
+    update_quality if item[:sell_in] < 11
+    update_quality if item[:sell_in] < 6
+
+    item[:sell_in] -= 1
+
+    update_quality(0) if item[:sell_in] == 0
+  end
+
+  def update_sulfuras_item
+    # nothing to do
+  end
+
+  def update_quality(val=1)
+    item[:quality] += val if item[:quality] < 50
+  end
 end
 
-# DO NOT CHANGE THINGS BELOW -----------------------------------------
-
-Item = Struct.new(:name, :sell_in, :quality)
-
-# We use the setup in the spec rather than the following for testing.
-#
-# Items = [
-#   Item[:new]("+5 Dexterity Vest", 10, 20),
-#   Item[:new]("Aged Brie", 2, 0),
-#   Item[:new]("Elixir of the Mongoose", 5, 7),
-#   Item[:new]("Sulfuras, Hand of Ragnaros", 0, 80),
-#   Item[:new]("Backstage passes to a TAFKAL80ETC concert", 15, 20),
-#   Item[:new]("Conjured Mana Cake", 3, 6),
-# ]
+def update_quality(items)
+  items.each do |item|
+    Item.new(item).update
+  end
+end
