b38720063f6c2fc6edecff30d7d43fa6d536fec9
Victor Martins <victor.martins@sage.com>
2017-11-16 08:22:39 +0000

    DRY

--- gilded_rose_kata/20171116/gilded_rose.rb
+++ gilded_rose_kata/20171116/gilded_rose.rb
@@ -19,42 +19,24 @@ class Item
   private
   attr_reader :item
 
+  def update_quality(val=1)
+    return if item[:quality] >= 50 || item[:quality] <= 0 # TODO: Range
+    item[:quality] += val
+  end
+
   def aged_product
-    if item[:quality] < 50
-      item[:quality] += 1
-    end
-
+    update_quality
     item[:sell_in] -= 1
-
-    if item[:sell_in] < 0
-      if item[:quality] < 50
-        item[:quality] += 1
-      end
-    end
+    update_quality if item[:sell_in] < 0
   end
 
   def backstage_product
-    if item[:quality] < 50
-      item[:quality] += 1
-    end
-
-    if item[:sell_in] < 11
-      if item[:quality] < 50
-        item[:quality] += 1
-      end
-    end
-
-    if item[:sell_in] < 6
-      if item[:quality] < 50
-        item[:quality] += 1
-      end
-    end
+    update_quality
+    update_quality if item[:sell_in] < 11
+    update_quality if item[:sell_in] < 6
 
     item[:sell_in] -= 1
-
-    if item[:sell_in] < 0
-      item[:quality] = 0
-    end
+    item[:quality] = 0 if item[:sell_in] < 0
   end
 
   def sulfuras_product
@@ -62,17 +44,9 @@ class Item
   end
 
   def default_product
-    if item[:quality] > 0
-      item[:quality] -= 1
-    end
-
+    update_quality(-1)
     item[:sell_in] -= 1
-
-    if item[:sell_in] < 0
-      if item[:quality] > 0
-        item[:quality] -= 1
-      end
-    end
+    update_quality(-1) if item[:sell_in] < 0
   end
 end
 
