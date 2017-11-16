2e56df7576a9a4941deafe01a51bb0119a534b56
Victor Martins <victor.martins@sage.com>
2017-11-16 07:48:09 +0000

    created class

---
+++ gilded_rose_kata/20171116/gilded_rose.rb
@@ -0,0 +1,60 @@
+class Item
+  def initialize(item)
+    @item = item
+  end
+
+  def update
+    if item[:name] != 'Aged Brie' && item[:name] != 'Backstage passes to a TAFKAL80ETC concert'
+      if item[:quality] > 0
+        if item[:name] != 'Sulfuras, Hand of Ragnaros'
+          item[:quality] -= 1
+        end
+      end
+    else
+      if item[:quality] < 50
+        item[:quality] += 1
+        if item[:name] == 'Backstage passes to a TAFKAL80ETC concert'
+          if item[:sell_in] < 11
+            if item[:quality] < 50
+              item[:quality] += 1
+            end
+          end
+          if item[:sell_in] < 6
+            if item[:quality] < 50
+              item[:quality] += 1
+            end
+          end
+        end
+      end
+    end
+    if item[:name] != 'Sulfuras, Hand of Ragnaros'
+      item[:sell_in] -= 1
+    end
+    if item[:sell_in] < 0
+      if item[:name] != "Aged Brie"
+        if item[:name] != 'Backstage passes to a TAFKAL80ETC concert'
+          if item[:quality] > 0
+            if item[:name] != 'Sulfuras, Hand of Ragnaros'
+              item[:quality] -= 1
+            end
+          end
+        else
+          item[:quality] = item[:quality] - item[:quality]
+        end
+      else
+        if item[:quality] < 50
+          item[:quality] += 1
+        end
+      end
+    end
+  end
+
+  private
+  attr_reader :item
+end
+
+def update_quality(items)
+  items.each do |item|
+    Item.new(item).update
+  end
+end
