adf97a3d6bf0e347add167e2dace764a210381dc
Victor Martins <victor.martins@sage.com>
2017-11-20 17:13:11 +0000

    normal product

--- gilded_rose_kata/20171120/gilded_rose.rb
+++ gilded_rose_kata/20171120/gilded_rose.rb
@@ -8,52 +8,8 @@ class Item
   def update
     return aged_product      if @name.downcase =~ /aged/
     return backstage_product if @name.downcase =~ /backstage/
-
-    if @name != 'Aged Brie' && @name != 'Backstage passes to a TAFKAL80ETC concert'
-      if @quality > 0
-        if @name != 'Sulfuras, Hand of Ragnaros'
-          @quality -= 1
-        end
-      end
-    else
-      if @quality < 50
-        @quality += 1
-        if @name == 'Backstage passes to a TAFKAL80ETC concert'
-          if @sell_in < 11
-            if @quality < 50
-              @quality += 1
-            end
-          end
-          if @sell_in < 6
-            if @quality < 50
-              @quality += 1
-            end
-          end
-        end
-      end
-    end
-    if @name != 'Sulfuras, Hand of Ragnaros'
-      @sell_in -= 1
-    end
-    if @sell_in < 0
-      if @name != "Aged Brie"
-        if @name != 'Backstage passes to a TAFKAL80ETC concert'
-          if @quality > 0
-            if @name != 'Sulfuras, Hand of Ragnaros'
-              @quality -= 1
-            end
-          end
-        else
-          @quality = @quality - @quality
-        end
-      else
-        if @quality < 50
-          @quality += 1
-        end
-      end
-    end
-
-    self
+    return sulfuras_product if @name.downcase =~ /sulfuras/
+    normal_product
   end
 
   def to_hash
@@ -65,6 +21,13 @@ class Item
   end
 
   private
+
+  def normal_product
+    update_quality(-1)
+    update_sell_in
+    update_quality(-1) if @sell_in < 0
+    self
+  end
 
   def aged_product
     update_quality
@@ -83,9 +46,13 @@ class Item
     self
   end
 
-  def update_quality
-    if @quality < 50
-      @quality += 1
+  def sulfuras_product
+    self
+  end
+
+  def update_quality(val=1)
+    if @quality < 50 && @quality > 0
+      @quality += val
     end
   end
 
