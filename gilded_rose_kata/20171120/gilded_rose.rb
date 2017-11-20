855273363bbb25131cbc6d93d41de8233d68763d
Victor Martins <victor.martins@sage.com>
2017-11-20 16:52:31 +0000

    added class

---
+++ gilded_rose_kata/20171120/gilded_rose.rb
@@ -0,0 +1,69 @@
+class Item
+  def initialize(item_data)
+    @name    = item_data[:name]
+    @quality = item_data[:quality]
+    @sell_in = item_data[:sell_in]
+  end
+
+  def update
+    if @name != 'Aged Brie' && @name != 'Backstage passes to a TAFKAL80ETC concert'
+      if @quality > 0
+        if @name != 'Sulfuras, Hand of Ragnaros'
+          @quality -= 1
+        end
+      end
+    else
+      if @quality < 50
+        @quality += 1
+        if @name == 'Backstage passes to a TAFKAL80ETC concert'
+          if @sell_in < 11
+            if @quality < 50
+              @quality += 1
+            end
+          end
+          if @sell_in < 6
+            if @quality < 50
+              @quality += 1
+            end
+          end
+        end
+      end
+    end
+    if @name != 'Sulfuras, Hand of Ragnaros'
+      @sell_in -= 1
+    end
+    if @sell_in < 0
+      if @name != "Aged Brie"
+        if @name != 'Backstage passes to a TAFKAL80ETC concert'
+          if @quality > 0
+            if @name != 'Sulfuras, Hand of Ragnaros'
+              @quality -= 1
+            end
+          end
+        else
+          @quality = @quality - @quality
+        end
+      else
+        if @quality < 50
+          @quality += 1
+        end
+      end
+    end
+
+    self
+  end
+
+  def to_hash
+    {
+      name:    @name,
+      quality: @quality,
+      sell_in: @sell_in
+    }
+  end
+end
+
+def update_quality(items)
+  items.map do |item|
+    Item.new(item).update.to_hash
+  end
+end
---
+++ gilded_rose_kata/20171120/spec/gilded_rose_spec.rb
@@ -0,0 +1,226 @@
+require 'spec_helper'
+require 'gilded_rose'
+
+RSpec.describe "#update_quality" do
+  context "with a single" do
+    let(:initial_sell_in) { 5 }
+    let(:initial_quality) { 10 }
+    let(:item) do
+      {
+        name:    name,
+        sell_in: initial_sell_in,
+        quality: initial_quality
+      }
+    end
+
+    subject { update_quality([item]).first }
+
+    context "normal item" do
+      let(:name) { "NORMAL ITEM" }
+
+      it { expect(subject[:sell_in]).to eq(initial_sell_in-1) }
+
+      it 'does not mutate the original item' do
+        expect(item[:sell_in]).to eq(initial_sell_in)
+      end
+
+      context "before sell date" do
+        it { expect(subject[:quality]).to eq(initial_quality-1) }
+      end
+
+      context "on sell date" do
+        let(:initial_sell_in) { 0 }
+        it { expect(subject[:quality]).to eq(initial_quality-2) }
+      end
+
+      context "after sell date" do
+        let(:initial_sell_in) { -10 }
+        it { expect(subject[:quality]).to eq(initial_quality-2) }
+      end
+
+      context "of zero quality" do
+        let(:initial_quality) { 0 }
+        it { expect(subject[:quality]).to eq(0) }
+      end
+    end
+
+    context "Aged Brie" do
+      let(:name) { "Aged Brie" }
+
+      it { expect(subject[:sell_in]).to eq(initial_sell_in-1) }
+
+      context "before sell date" do
+        it { expect(subject[:quality]).to eq(initial_quality+1) }
+
+        context "with max quality" do
+          let(:initial_quality) { 50 }
+          it { expect(subject[:quality]).to eq(initial_quality) }
+        end
+      end
+
+      context "on sell date" do
+        let(:initial_sell_in) { 0 }
+        it { expect(subject[:quality]).to eq(initial_quality+2) }
+
+        context "near max quality" do
+          let(:initial_quality) { 49 }
+          it { expect(subject[:quality]).to eq(50) }
+        end
+
+        context "with max quality" do
+          let(:initial_quality) { 50 }
+          it { expect(subject[:quality]).to eq(initial_quality) }
+        end
+      end
+
+      context "after sell date" do
+        let(:initial_sell_in) { -10 }
+        it { expect(subject[:quality]).to eq(initial_quality+2) }
+
+        context "with max quality" do
+          let(:initial_quality) { 50 }
+          it { expect(subject[:quality]).to eq(initial_quality) }
+        end
+      end
+    end
+
+    context "Sulfuras" do
+      let(:initial_quality) { 80 }
+      let(:name) { "Sulfuras, Hand of Ragnaros" }
+
+      it { expect(subject[:sell_in]).to eq(initial_sell_in) }
+
+      context "before sell date" do
+        it { expect(subject[:quality]).to eq(initial_quality) }
+      end
+
+      context "on sell date" do
+        let(:initial_sell_in) { 0 }
+        it { expect(subject[:quality]).to eq(initial_quality) }
+      end
+
+      context "after sell date" do
+        let(:initial_sell_in) { -10 }
+        it { expect(subject[:quality]).to eq(initial_quality) }
+      end
+    end
+
+    context "Backstage pass" do
+      let(:name) { "Backstage passes to a TAFKAL80ETC concert" }
+
+      it { expect(subject[:sell_in]).to eq(initial_sell_in-1) }
+
+      context "long before sell date" do
+        let(:initial_sell_in) { 11 }
+        it { expect(subject[:quality]).to eq(initial_quality+1) }
+
+        context "at max quality" do
+          let(:initial_quality) { 50 }
+        end
+      end
+
+      context "medium close to sell date (upper bound)" do
+        let(:initial_sell_in) { 10 }
+        it { expect(subject[:quality]).to eq(initial_quality+2) }
+
+        context "at max quality" do
+          let(:initial_quality) { 50 }
+          it { expect(subject[:quality]).to eq(initial_quality) }
+        end
+      end
+
+      context "medium close to sell date (lower bound)" do
+        let(:initial_sell_in) { 6 }
+        it { expect(subject[:quality]).to eq(initial_quality+2) }
+
+        context "at max quality" do
+          let(:initial_quality) { 50 }
+          it { expect(subject[:quality]).to eq(initial_quality) }
+        end
+      end
+
+      context "very close to sell date (upper bound)" do
+        let(:initial_sell_in) { 5 }
+        it { expect(subject[:quality]).to eq(initial_quality+3) }
+
+        context "at max quality" do
+          let(:initial_quality) { 50 }
+          it { expect(subject[:quality]).to eq(initial_quality) }
+        end
+      end
+
+      context "very close to sell date (lower bound)" do
+        let(:initial_sell_in) { 1 }
+        it { expect(subject[:quality]).to eq(initial_quality+3) }
+
+        context "at max quality" do
+          let(:initial_quality) { 50 }
+          it { expect(subject[:quality]).to eq(initial_quality) }
+        end
+      end
+
+      context "on sell date" do
+        let(:initial_sell_in) { 0 }
+        it { expect(subject[:quality]).to eq(0) }
+      end
+
+      context "after sell date" do
+        let(:initial_sell_in) { -10 }
+        it { expect(subject[:quality]).to eq(0) }
+      end
+    end
+
+    xcontext "conjured item" do
+      let(:name) { "Conjured Mana Cake" }
+
+      it { expect(subject[:sell_in]).to eq(initial_sell_in-1) }
+
+      context "before the sell date" do
+        let(:initial_sell_in) { 5 }
+        it { expect(subject[:quality]).to eq(initial_quality-2) }
+
+        context "at zero quality" do
+          let(:initial_quality) { 0 }
+          it { expect(subject[:quality]).to eq(initial_quality) }
+        end
+      end
+
+      context "on sell date" do
+        let(:initial_sell_in) { 0 }
+        it { expect(subject[:quality]).to eq(initial_quality-4) }
+
+        context "at zero quality" do
+          let(:initial_quality) { 0 }
+          it { expect(subject[:quality]).to eq(initial_quality) }
+        end
+      end
+
+      context "after sell date" do
+        let(:initial_sell_in) { -10 }
+        it { expect(subject[:quality]).to eq(initial_quality-4) }
+
+        context "at zero quality" do
+          let(:initial_quality) { 0 }
+          it { expect(subject[:quality]).to eq(initial_quality) }
+        end
+      end
+    end
+  end
+
+  context "with several objects" do
+    let(:items) {
+      [
+        {name: "NORMAL ITEM", sell_in: 5, quality: 10},
+        {name: "Aged Brie", sell_in: 3, quality: 10},
+      ]
+    }
+
+    subject { update_quality(items) }
+
+    it { expect(subject[0][:quality]).to eq(9) }
+    it { expect(subject[0][:sell_in]).to eq(4) }
+
+    it { expect(subject[1][:quality]).to eq(11) }
+    it { expect(subject[1][:sell_in]).to eq(2) }
+  end
+end
