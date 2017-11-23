3dce718ec882e28b937209663d27021734894b17
Victor Martins <victor.martins@sage.com>
2017-11-23 18:38:22 +0000

    done

--- roman_numerals/20171123/lib/roman_numerals_converter.rb
+++ roman_numerals/20171123/lib/roman_numerals_converter.rb
@@ -16,5 +16,50 @@ class RomanNumeralsConverter
   }
 
   def convert(number)
+    return arabic_to_roman(number) if number.is_a?(Integer)
+    roman_to_arabic(number)
+  end
+
+  private
+
+  def arabic_to_roman(number)
+    return convert_arabic_to_roman(number) if number >= 0
+    "-#{convert_arabic_to_roman(-number)}"
+  end
+
+  def convert_arabic_to_roman(number)
+    result = ''
+    ROMAN_NUMERALS.each_pair do |roman, arabic|
+      while number >= arabic do
+        # break if too many iterations
+        number -= arabic
+        result << roman
+      end
+    end
+    result
+  end
+
+  def roman_to_arabic(number)
+    return -convert_roman_to_arabic(number.sub('-', '')) if number =~ /^-/
+    convert_roman_to_arabic(number)
+  end
+
+  def convert_roman_to_arabic(number)
+    result = 0
+    number.chars.each.with_index do |char, index|
+      current_n = char_to_number(char)
+      next_n = char_to_number(number[index + 1])
+
+      if next_n > current_n
+        result -= current_n
+      else
+        result += current_n
+      end
+    end
+    result
+  end
+
+  def char_to_number(char)
+    ROMAN_NUMERALS[char] || 0
   end
 end
