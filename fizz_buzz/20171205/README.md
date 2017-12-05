48dfbe97e0821f462f83f20b13492cab4db79461
Victor Martins <victor.martins@sage.com>
2017-12-05 08:02:04 +0000

    start

---
+++ fizz_buzz/20171205/README.md
@@ -0,0 +1,11 @@
+http://codingdojo.org/kata/FizzBuzz/
+http://www.codekatas.org/casts/code-kata-fizzbuzz-by-lets-develop
+
+
+Rules:
+Output "Fizz" if number is dividable by 3
+Output "Buzz" if number is dividable by 5
+Output "FizzBuzz" if number is dividable by 3 and 5
+Output number if otherwise.
+
+Output a list from from 1 to 100
---
+++ fizz_buzz/20171205/spec/fizz_buzz_spec.rb
@@ -0,0 +1,34 @@
+require_relative './../lib/fizz_buzz'
+
+RSpec.describe FizzBuzz do
+  # let(:input) { (1..100) }
+  # let(:output) do
+
+  # end
+
+  it 'returns "fizz" if number is dividable by 3' do
+    expect(subject.call(3)).to eq('Fizz')
+  end
+
+  it 'returns "Buzz" if number is dividable by 5' do
+    expect(subject.call(5)).to eq('Buzz')
+  end
+
+  it 'returns "FizzBuzz" if the number is dividable by 3 and 5' do
+    expect(subject.call(15)).to eq('FizzBuzz')
+  end
+
+  it 'returns the number otherwise' do
+    expect(subject.call(4)).to eq(4)
+  end
+
+  it 'returns an array if it receives an array' do
+    input = (1..20)
+    output = [
+      1,  2,  'Fizz',     4,      'Buzz', 'Fizz',
+      7,  8,  'Fizz',     'Buzz',  11,    'Fizz',
+      13, 14, 'FizzBuzz', 16, 17, 'Fizz',  19, 'Buzz'
+    ]
+    expect(subject.call(input)).to eq(ouput)
+  end
+end
