b5710275637a82830db805f1ca82c25316bfd33f
Victor Martins <victor.martins@sage.com>
2017-12-06 07:18:31 +0000

    done

--- fizz_buzz/20171206/lib/fizz_buzz.rb
+++ fizz_buzz/20171206/lib/fizz_buzz.rb
@@ -1,4 +1,28 @@
 class FizzBuzz
-  def call(numbers)
+  FIZZ = 'Fizz'
+  BUZZ = 'Buzz'
+
+  def initialize(options={})
+    @extra = options.fetch(:extra) { false }
+  end
+
+  def call(input)
+    return process(input) if input.is_a?(Integer)
+    input.map { |n| process(n) }
+  end
+
+  def process(number)
+    return FIZZ+BUZZ if div_by?(number, 3, 5)
+    return FIZZ if div_by?(number, 3)
+    return BUZZ if div_by?(number, 5)
+    return FIZZ if number.to_s =~ /3/ if @extra
+    return BUZZ if number.to_s =~ /5/ if @extra
+    number
+  end
+
+  private
+
+  def div_by?(number, *dividers)
+    dividers.map { |d| number % d == 0 }.all?
   end
 end
