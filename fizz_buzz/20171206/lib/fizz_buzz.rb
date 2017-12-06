0b0b4934a3707b5396c16527dedbf9809dc60a86
Victor Martins <victor.martins@sage.com>
2017-12-06 07:39:40 +0000

    refactoring using Reek advises

--- fizz_buzz/20171206/lib/fizz_buzz.rb
+++ fizz_buzz/20171206/lib/fizz_buzz.rb
@@ -1,3 +1,4 @@
+# Class to encapsulate the FizzBuzz Logic
 class FizzBuzz
   FIZZ = 'Fizz'
   BUZZ = 'Buzz'
@@ -8,21 +9,31 @@ class FizzBuzz
 
   def call(input)
     return process(input) if input.is_a?(Integer)
-    input.map { |n| process(n) }
-  end
-
-  def process(number)
-    return FIZZ+BUZZ if div_by?(number, 3, 5)
-    return FIZZ if div_by?(number, 3)
-    return BUZZ if div_by?(number, 5)
-    return FIZZ if number.to_s =~ /3/ if @extra
-    return BUZZ if number.to_s =~ /5/ if @extra
-    number
+    input.map { |number| process(number) }
   end
 
   private
 
+  def process(number)
+    by_division(number) || by_match(number) || number
+  end
+
+  def by_division(number)
+    return FIZZ+BUZZ if div_by?(number, 3, 5)
+    return FIZZ if div_by?(number, 3)
+    return BUZZ if div_by?(number, 5)
+  end
+
+  def by_match(number)
+    return FIZZ if has_digit?(number, 3) if @extra
+    return BUZZ if has_digit?(number, 5) if @extra
+  end
+
   def div_by?(number, *dividers)
-    dividers.map { |d| number % d == 0 }.all?
+    dividers.map { |divider| number % divider == 0 }.all?
+  end
+
+  def has_digit?(number, digit)
+    number.to_s =~ /#{Regexp.quote(digit.to_s)}/
   end
 end
