58f1fb571598540b66f1f38b71b59ac8f0b846d1
Victor Martins <victor.martins@sage.com>
2017-12-14 18:33:22 +0000

    done

--- roman_numerals/20171214/lib/roman_numerals_converter.rb
+++ roman_numerals/20171214/lib/roman_numerals_converter.rb
@@ -20,6 +20,55 @@ class RomanNumeralsConverter
     'I'  => 1
   }
 
+  Contract(Or[String, Num] => Or[String, Num])
   def convert(number)
+    return arabic_to_roman(number) if number.is_a?(Integer)
+    roman_to_arabic(number)
+  end
+
+  private
+
+  Contract(Num => String)
+  def arabic_to_roman(number)
+    return convert_arabic_to_roman(number) if number >= 0
+    "-#{convert_arabic_to_roman(-number)}"
+  end
+
+  def convert_arabic_to_roman(number)
+    ROMAN_NUMERALS.each_pair.with_object('') do |roman_arabic, result|
+      arabic = roman_arabic[1]
+      roman  = roman_arabic[0]
+
+      while number >= arabic
+        number -= arabic
+        result << roman
+      end
+    end
+  end
+
+  Contract(String => Num)
+  def roman_to_arabic(number)
+    return -convert_roman_to_arabic(number.sub('-', '')) if number =~ /^-/
+    convert_roman_to_arabic(number)
+  end
+
+  def convert_roman_to_arabic(number)
+    number.chars.each.with_index.inject(0) do |result, char_index|
+      char      = char_index[0]
+      index     = char_index[1]
+      current_n = char_to_number(char)
+      next_n    = char_to_number(number[index + 1])
+
+      if next_n > current_n
+        result -= current_n
+      else
+        result += current_n
+      end
+    end
+  end
+
+  Contract(Maybe[String] => Num)
+  def char_to_number(char)
+    ROMAN_NUMERALS[char] || 0
   end
 end
