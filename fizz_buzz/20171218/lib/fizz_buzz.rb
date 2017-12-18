a691e6df8afa8f3bf85ed80c466c86f4632bf989
Victor Martins <victor.martins@sage.com>
2017-12-18 12:57:54 +0000

    done

--- fizz_buzz/20171218/lib/fizz_buzz.rb
+++ fizz_buzz/20171218/lib/fizz_buzz.rb
@@ -1,4 +1,32 @@
 class FizzBuzz
+  FIZZ = 'Fizz'.freeze
+  BUZZ = 'Buzz'.freeze
+
+  def initialize(options={})
+    @extra = options.fetch(:extra) { false }
+  end
+
   def call(input)
+    return process_number(input) if input.is_a?(Numeric)
+    input.map { |number| process_number(number) }
+  end
+
+  def process_number(input)
+    return FIZZ + BUZZ if div_by?(input, 3, 5)
+    return FIZZ if div_by?(input, 3)
+    return BUZZ if div_by?(input, 5)
+    return FIZZ if has_digit?(input, 3) if @extra
+    return BUZZ if has_digit?(input, 5) if @extra
+    input
+  end
+
+  private
+
+  def div_by?(number, *divs)
+    divs.map { |div| number % div == 0 }.all?
+  end
+
+  def has_digit?(input, digit)
+    input.to_s.include?(digit.to_s)
   end
 end
