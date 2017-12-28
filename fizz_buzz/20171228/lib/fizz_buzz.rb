6c3de43c87a23864565392faff9153ec00e319a7
Victor Martins <victor.martins@sage.com>
2017-12-28 17:31:28 +0000

    done

--- fizz_buzz/20171228/lib/fizz_buzz.rb
+++ fizz_buzz/20171228/lib/fizz_buzz.rb
@@ -1,4 +1,38 @@
+class FizzBuzzRules
+  def initialize(input)
+    @input = input
+  end
+
+  def div_by?(*divs)
+    divs.map { |div| @input % div == 0 }.all?
+  end
+
+  def has_digit?(digit)
+    @input.to_s.include?(digit.to_s)
+  end
+end
+
 class FizzBuzz
+  FIZZ = 'Fizz'.freeze
+  BUZZ = 'Buzz'.freeze
+
+  def initialize(options={})
+    @check_digit = options[:check_digit]
+  end
+
   def call(input)
+    return process(input) if input.is_a?(Integer)
+    input.map { |number| process(number) }
+  end
+
+  def process(input)
+    rules = FizzBuzzRules.new(input)
+
+    return FIZZ + BUZZ if rules.div_by?(3, 5)
+    return FIZZ if rules.div_by?(3)
+    return BUZZ if rules.div_by?(5)
+    return FIZZ if rules.has_digit?(3) if @check_digit
+    return BUZZ if rules.has_digit?(5) if @check_digit
+    input
   end
 end
