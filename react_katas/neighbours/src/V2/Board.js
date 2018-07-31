6beedd757cdb47dc5511db4c546b2b265c537931
Victor Martins <victor.martins@sage.com>
2018-07-31 21:37:35 +0100

    Highlighting neighbours but buggy

--- react_katas/neighbours/src/V2/Board.js
+++ react_katas/neighbours/src/V2/Board.js
@@ -7,22 +7,28 @@ export default class Board extends React.Component {
     }
 
     renderBoard = () => {
-        const { boardHeight } = this.props
+        const { boardWidth } = this.props
         const { boardData } = this.state
 
         const groupCellsIntoLines = (result, cellData, index) => {
-            const breakLine = index % boardHeight == 0
+            const breakLine = index % boardWidth == 0
+            const cellID = `${cellData.x}_${cellData.y}`
+
             if (breakLine) {
                 result.push([])
             }
             result[result.length - 1].push(
-                <Cell key={`${cellData.x}_${cellData.y}`} color={cellData.color} highLighted={cellData.highLighted} />
+                <Cell key={cellID} cellID={cellID} color={cellData.color} highLighted={cellData.highLighted} />
             )
             return result
         }
 
         const insertIntoDivs = (cells, index) => {
-            return <div key={index}>{cells}</div>
+            return (
+                <div className="cellContainer" key={index}>
+                    {cells}
+                </div>
+            )
         }
 
         const result = boardData.reduce(groupCellsIntoLines, []).map(insertIntoDivs)
@@ -39,6 +45,7 @@ export default class Board extends React.Component {
     generateBoardData = () => {
         const result = []
         const { boardWidth, boardHeight } = this.props
+
         for (let h = 0; h < boardHeight; h++) {
             for (let w = 0; w < boardWidth; w++) {
                 result.push({
@@ -61,17 +68,23 @@ export default class Board extends React.Component {
         this.setState({ boardData: this.generateBoardData() })
     }
 
+    // group neighbour groups by color
+    // a color is an array of arrays, each array has grouped cells by proximity
+    // we keep track of which neighbourhood has more neighbours so that we can easily find the one we want later
     findNeighbours = () => {
         const groupColorsByNeighbours = (result, cell) => {
             result[cell.color] = result[cell.color] || []
+            result[cell.color].max = 0
             // console.log('RESULT:', result)
             // console.log(`RESULT[${cell.color}]:`, result[cell.color])
             // console.log('CELL:', cell)
 
+            // if any (array#some) of the arrays has a neighbour of this cell, add it to that group
+            // otherwise create a new group (array)
             if (
                 result[cell.color].some((groupOfNeighbours) => {
                     const areNeighbours = groupOfNeighbours.some((cellInGroupOfNeighbours) => {
-                        console.log('currentCell', cell, 'cellInGroupOfNeighbours', cellInGroupOfNeighbours)
+                        // console.log('currentCell', cell, 'cellInGroupOfNeighbours', cellInGroupOfNeighbours)
                         return (
                             // TOP AND BOTTOM
                             (cell.x === cellInGroupOfNeighbours.x && cell.y === cellInGroupOfNeighbours.y - 1) ||
@@ -87,23 +100,57 @@ export default class Board extends React.Component {
                             (cell.x === cellInGroupOfNeighbours.x + 1 && cell.y === cellInGroupOfNeighbours.y + 1)
                         )
                     })
+
                     if (areNeighbours) {
-                        console.log('Push Neighbour to group')
+                        // console.log('Push Neighbour to group')
                         groupOfNeighbours.push(cell)
+                    }
+
+                    if (result[cell.color].max < groupOfNeighbours.length) {
+                        result[cell.color].max = groupOfNeighbours.length
                     }
 
                     return areNeighbours
                 })
             ) {
-                console.log('Cell Added')
+                // console.log('Cell Added')
             } else {
                 result[cell.color].push([cell])
-                console.log('New Neighbour Group Created')
+                // console.log('New Neighbour Group Created')
             }
             return result
         }
-        const result = this.state.boardData.reduce(groupColorsByNeighbours, {})
-        console.log('NEIGHBOURS:', result)
+
+        const colorsAndNeighbours = this.state.boardData.reduce(groupColorsByNeighbours, {})
+        // TODO: VICMAR It seems that these sorts are buggy
+        const maxNeighbours = Object.entries(colorsAndNeighbours)
+            .sort((allColorsGroupsTuplesA, allColorsGroupsTuplesB) => {
+                // console.log('allColorsGroupsTuplesA', allColorsGroupsTuplesA[0], allColorsGroupsTuplesA[1])
+                // console.log('allColorsGroupsTuplesB', allColorsGroupsTuplesB[0], allColorsGroupsTuplesB[1])
+                return allColorsGroupsTuplesB[1].max - allColorsGroupsTuplesA[1].max
+            })[0]
+            .sort((colorGroupTuple) => {
+                colorGroupTuple[1].length
+            })
+        const neighboursToHighlight = maxNeighbours[1][0]
+        console.log('Max NEIGHBOURS:', maxNeighbours)
+        console.log('MaxToHighLight:', neighboursToHighlight)
+
+        const { boardData } = this.state
+
+        for (const cell of neighboursToHighlight) {
+            const cellToHighlight = boardData.find((c) => {
+                return c.x === cell.x && c.y === cell.y
+            })
+            cellToHighlight.highLighted = true
+        }
+        const highLightedBoardData = boardData.reduce((result, cell) => {
+            console.log('Data', cell)
+            return result
+        }, {})
+
+        this.setState({ boardData: boardData })
+        console.log('highLightedBoardData', highLightedBoardData)
     }
 
     render() {
--- react_katas/neighbours/src/V2/Cell.js
+++ react_katas/neighbours/src/V2/Cell.js
@@ -3,15 +3,20 @@ import styled, { css } from 'styled-components'
 
 export default class Cell extends React.Component {
     render() {
-        return <Div className="cell" color={this.props.color} highLighted={this.props.highLighted} />
+        return (
+            <Div className="cell" color={this.props.color} highLighted={this.props.highLighted}>
+                <span>{this.props.cellID}</span>
+            </Div>
+        )
     }
 }
 
 const Div = styled.div`
+    color: white;
     margin: 2px;
     padding: 10px;
-    width: 10px;
-    height: 10px;
+    width: 30px;
+    height: 30px;
     background-color: ${(props) => props.color};
     border: 4px solid white;
     ${(props) =>
