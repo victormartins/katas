7a7b167b3708f05c7b690dd70f17b5d95599a0c6
Victor Martins <victor.martins@sage.com>
2018-12-10 13:05:01 +0000

    Arabic to Roman

--- ruby_katas/roman_numerals/20181209/lib/roman_numerals_converter.rb
+++ ruby_katas/roman_numerals/20181209/lib/roman_numerals_converter.rb
@@ -21,5 +21,44 @@ class RomanNumeralsConverter
   }
 
   def convert(number)
+    require 'logger'
+    @logger = Logger.new(STDOUT)
+
+    if(number < 0)
+      number *= -1
+      result = convert_arabic_to_roman(number)
+      result = "-#{result}"
+    else
+      result = convert_arabic_to_roman(number)
+    end
+    result
+  end
+
+  private
+
+  attr_reader :logger
+
+  # Example:
+  # 20
+  # find closest <= 20 in roman...
+  # Found X
+  # add X to result
+  # subtract 10
+
+  # 10
+  # find closest <= 10 in roman...
+  # Found X
+  # add X to result
+  # subtract 10
+  def convert_arabic_to_roman(number)
+    result = ''
+    leftover = number
+    while(leftover > 0) do
+      key_value = ROMAN_NUMERALS.each_pair.detect { |k, v| v <= leftover }
+      result << key_value[0]
+      leftover = leftover - key_value[1]
+    end
+    result
   end
 end
+
