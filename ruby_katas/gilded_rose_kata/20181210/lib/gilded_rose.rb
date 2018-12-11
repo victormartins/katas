8a4906b5370796c105338c3f70bdc3ee2771e5e7
Victor Martins <victor.martins@sage.com>
2018-12-11 09:47:48 +0000

    done

--- ruby_katas/gilded_rose_kata/20181210/lib/gilded_rose.rb
+++ ruby_katas/gilded_rose_kata/20181210/lib/gilded_rose.rb
@@ -1,47 +1,77 @@
-def update_quality(items)
-  items.each do |item|
-    if item[:name] != 'Aged Brie' && item[:name] != 'Backstage passes to a TAFKAL80ETC concert'
-      if item[:quality] > 0
-        if item[:name] != 'Sulfuras, Hand of Ragnaros'
-          item[:quality] -= 1
-        end
-      end
+class UpdateItem
+  def call(item_data)
+    item_name = item_data.fetch(:name)
+
+    return update_backstage(item_data) if item_name =~ /Backstage/
+    return update_brie(item_data) if item_name =~ /Brie/
+    return update_sulfuras(item_data) if item_name =~ /Sulfuras/
+    return update_conjured(item_data) if item_name =~ /Conjured/
+    update_normal_item(item_data)
+  end
+
+  private
+
+  def update_backstage(item_data)
+    update_quality(item_data)
+    update_quality(item_data) if (item_data.fetch(:sell_in) < 11)
+    update_quality(item_data) if (item_data.fetch(:sell_in) < 6)
+    item_data[:quality] = 0   if (item_data.fetch(:sell_in) <= 0 )
+    item_data[:sell_in] -= 1
+  end
+
+  def update_brie(item_data)
+    update_quality(item_data)
+    update_quality(item_data) if (item_data.fetch(:sell_in) <= 0)
+    item_data[:sell_in] -= 1
+  end
+
+  def update_sulfuras(item_data)
+    # nothing to do
+    item_data
+  end
+
+  def update_conjured(item_data)
+    update_quality(item_data, increment: false)
+    update_quality(item_data, increment: false)
+
+    if (item_data.fetch(:sell_in) <= 0)
+      update_quality(item_data, increment: false)
+      update_quality(item_data, increment: false)
+    end
+
+    item_data[:sell_in] -= 1
+  end
+
+  def update_normal_item(item_data)
+    item_data[:sell_in] -= 1
+    update_quality(item_data, increment: false)
+
+    if(item_data.fetch(:sell_in) <= 0)
+      update_quality(item_data, increment: false)
+    end
+  end
+
+  def update_quality(item_data, increment: true)
+    if(increment)
+      item_data[:quality] += 1
     else
-      if item[:quality] < 50
-        item[:quality] += 1
-        if item[:name] == 'Backstage passes to a TAFKAL80ETC concert'
-          if item[:sell_in] < 11
-            if item[:quality] < 50
-              item[:quality] += 1
-            end
-          end
-          if item[:sell_in] < 6
-            if item[:quality] < 50
-              item[:quality] += 1
-            end
-          end
-        end
-      end
+      item_data[:quality] -= 1
     end
-    if item[:name] != 'Sulfuras, Hand of Ragnaros'
-      item[:sell_in] -= 1
+
+    if(item_data.fetch(:quality) <= 0)
+      item_data[:quality] = 0
     end
-    if item[:sell_in] < 0
-      if item[:name] != "Aged Brie"
-        if item[:name] != 'Backstage passes to a TAFKAL80ETC concert'
-          if item[:quality] > 0
-            if item[:name] != 'Sulfuras, Hand of Ragnaros'
-              item[:quality] -= 1
-            end
-          end
-        else
-          item[:quality] = item[:quality] - item[:quality]
-        end
-      else
-        if item[:quality] < 50
-          item[:quality] += 1
-        end
-      end
+
+    if(item_data.fetch(:quality) > 50)
+      item_data[:quality] = 50
     end
+  end
+end
+
+def update_quality(items)
+  update_item = UpdateItem.new
+
+  items.each do |item|
+   update_item.call(item)
   end
 end
