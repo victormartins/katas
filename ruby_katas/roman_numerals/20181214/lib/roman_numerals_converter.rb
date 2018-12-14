09bd94aa1018558f8e1cc65a5ff127cb19b246c8
Victor Martins <victor.martins@sage.com>
2018-12-14 13:31:39 +0000

    done

--- ruby_katas/roman_numerals/20181214/lib/roman_numerals_converter.rb
+++ ruby_katas/roman_numerals/20181214/lib/roman_numerals_converter.rb
@@ -1,5 +1,65 @@
 require 'contracts'
 
+class ArabicToRoman
+  def initialize(roman_numerals)
+    @roman_numerals = roman_numerals
+  end
+
+  def call(number)
+    return execute(number) if number >= 0
+
+    result = execute(number * -1)
+    "-#{result}"
+  end
+
+  private
+
+  def execute(number)
+    remainer = number
+    result = ''
+
+    while remainer > 0
+      roman_arabic_pair = @roman_numerals.detect { |_k, value|  value <= remainer  }
+      roman = roman_arabic_pair[0]
+      value = roman_arabic_pair[1]
+      result << roman
+      remainer -= value
+    end
+
+    result
+
+  end
+end
+
+class RomanToArabic
+  def initialize(roman_numerals)
+    @roman_numerals = roman_numerals
+  end
+
+  def call(roman_number)
+    return execute(roman_number) unless roman_number.start_with?('-')
+    result = execute(roman_number.sub('-', ''))
+    result * -1
+  end
+
+  private
+
+  def execute(roman_number)
+    result = 0
+    roman_number.chars.each.with_index do |char, index|
+      value = @roman_numerals[char]
+      next_value = @roman_numerals[roman_number[index + 1]]
+
+      if(next_value && next_value > value)
+        result -= value
+      else
+        result += value
+      end
+    end
+    result
+  end
+end
+
 class RomanNumeralsConverter
   include Contracts::Core
   include Contracts::Builtin
@@ -21,5 +81,7 @@ class RomanNumeralsConverter
   }
 
   def convert(number)
+    return ArabicToRoman.new(ROMAN_NUMERALS).call(number) if number.kind_of?(Numeric)
+    RomanToArabic.new(ROMAN_NUMERALS).call(number)
   end
 end
