bd9c32ed00bd7e1726397ec827bee85131a3e67b
Victor Martins <victor.martins@sage.com>
2019-01-05 00:25:45 +0000

    done

--- ruby_katas/roman_numerals/20190105/lib/roman_numerals_converter.rb
+++ ruby_katas/roman_numerals/20190105/lib/roman_numerals_converter.rb
@@ -1,9 +1,6 @@
 require 'contracts'
 
-class RomanNumeralsConverter
-  include Contracts::Core
-  include Contracts::Builtin
-
+module RomanNumerals
   ROMAN_NUMERALS = {
     'M'  => 1000,
     'CM' => 900,
@@ -19,7 +16,74 @@ class RomanNumeralsConverter
     'IV' => 4,
     'I'  => 1
   }
+end
+
+class ArabicToRoman
+  include RomanNumerals
+
+  def call(number)
+    return execute(number) if number >=0
+    result = execute(number * -1)
+    "-#{result}"
+  end
+
+  private
+
+  def execute(number)
+    remainder = number
+    result = ''
+
+    while remainder > 0
+      char_val_tupple = ROMAN_NUMERALS.detect do |char, val|
+        val <= remainder
+      end
+
+      char = char_val_tupple[0]
+      val = char_val_tupple[1]
+
+      remainder -= val
+      result << char
+    end
+
+    result
+  end
+end
+
+class RomanToNumeric
+  include RomanNumerals
+
+  def call(roman)
+    return execute(roman) unless roman.start_with?('-')
+    result = execute(roman.sub('-', ''))
+    result * -1
+  end
+
+  private
+
+  def execute(roman)
+    result = 0
+    roman.chars.each.with_index do |char, index|
+      val = ROMAN_NUMERALS[char]
+      next_val = ROMAN_NUMERALS[roman[index + 1]]
+
+      if(next_val && next_val > val)
+        result -= val
+      else
+        result += val
+      end
+
+    end
+    result
+  end
+end
+
+class RomanNumeralsConverter
+  include Contracts::Core
+  include Contracts::Builtin
+
 
   def convert(number)
+    return ArabicToRoman.new.(number) if number.kind_of?(Numeric)
+    RomanToNumeric.new.(number)
   end
 end
