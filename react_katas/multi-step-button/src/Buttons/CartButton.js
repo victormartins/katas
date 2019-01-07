9e4136c89fb5103483edfdc21a2c4b24fa4e6944
Victor Martins <victor.martins@sage.com>
2019-01-07 12:54:16 +0000

    styling

--- react_katas/multi-step-button/src/Buttons/CartButton.js
+++ react_katas/multi-step-button/src/Buttons/CartButton.js
@@ -21,15 +21,32 @@ const Cart = styled.div`
   }
 
   li{
-    padding-top: 1em;
-    padding-bottom: 0.3em;
+    padding: 1em 1em 0.3em 1em;
     border-bottom: 1px solid black;
     cursor: pointer;
+    span {
+      border-bottom: 1px solid black;
+    }
+
+    background-color: orange;
+    margin: 0.4em;
+
+    text-align: left;
+    display: flex;
+    justify-content: space-between;
+
+    box-shadow: -3px 3px 3px;
   }
 
   li:hover{
     background-color: black;
     color: white;
+
+    span {
+      background-color: blue;
+      border: 1px solid;
+      padding: 0.2em;
+    }
   }
 `
 
@@ -56,8 +73,8 @@ class CartButton extends React.Component {
                 <CartBtn onClick={this.handleClick}>Shopping Cart</CartBtn>
                 <Cart open={this.state.open}>
                     <ul>
-                        <li>Item 1</li>
-                        <li>Item 2</li>
+                        <li>Some Item <span>£10.00</span></li>
+                        <li>Another Item <span>£12.00</span></li>
                     </ul>
                 </Cart>
             </div>
