64ca6c1380633f727c40cca3ec6284c4ffb59c72
Victor Martins <victor.martins@sage.com>
2019-03-07 10:30:02 +0000

    done

--- ruby_katas/roman_numerals/20190307/lib/roman_numerals_converter.rb
+++ ruby_katas/roman_numerals/20190307/lib/roman_numerals_converter.rb
@@ -1,9 +1,6 @@
 require 'contracts'
 
-class RomanNumeralsConverter
-  include Contracts::Core
-  include Contracts::Builtin
-
+module RomanNumerals
   ROMAN_NUMERALS = {
     'M'  => 1000,
     'CM' => 900,
@@ -19,7 +16,72 @@ class RomanNumeralsConverter
     'IV' => 4,
     'I'  => 1
   }
+end
+
+class RomanNumeralsConverter
+  include Contracts::Core
+  include Contracts::Builtin
 
   def convert(number)
+    return ArabicToRoman.new.(number) if number.kind_of?(Numeric)
+    RomanToArabic.new.(number)
+  end
+end
+
+class ArabicToRoman
+  include RomanNumerals
+
+  def call(number)
+    return execute(number) if number >= 0
+    "-#{execute(-number)}"
+  end
+
+  private
+
+  def execute(number)
+    result = ''
+    remainder = number
+
+    while(remainder > 0) do
+      roman_value = ROMAN_NUMERALS.find do |r_v|
+        v = r_v[1]
+        v <= remainder
+      end
+
+      roman = roman_value[0]
+      value = roman_value[1]
+
+      result << roman
+      remainder -= value
+    end
+
+    result
+  end
+end
+
+class RomanToArabic
+  include RomanNumerals
+
+  def call(roman)
+    return execute(roman) unless roman.start_with?('-')
+    -execute(roman.sub('-', ''))
+  end
+
+  private
+
+  def execute(roman)
+    roman.chars.each.with_index.reduce(0) do |result, char_index|
+      char = char_index[0]
+      index = char_index[1]
+
+      val = ROMAN_NUMERALS[char]
+      next_val = ROMAN_NUMERALS[roman[index + 1]]
+
+      if(next_val && next_val > val)
+        result -= val
+      else
+        result += val
+      end
+    end
   end
 end
