e263eab202716542b8560903de84a83befbbd510
Victor Guiomar Martins <correio@victormartins.com>
2019-04-15 10:22:40 +0100

    done

--- ruby_katas/roman_numerals/20190415/lib/roman_numerals_converter.rb
+++ ruby_katas/roman_numerals/20190415/lib/roman_numerals_converter.rb
@@ -1,9 +1,6 @@
 require 'contracts'
 
-class RomanNumeralsConverter
-  include Contracts::Core
-  include Contracts::Builtin
-
+module RomanNumerals
   ROMAN_NUMERALS = {
     'M'  => 1000,
     'CM' => 900,
@@ -20,6 +17,78 @@ class RomanNumeralsConverter
     'I'  => 1
   }
 
+end
+
+class RomanNumeralsConverter
+  include Contracts::Core
+  include Contracts::Builtin
+
+  attr_reader :arabic_to_roman, :roman_to_arabic
+
+  def initialize
+    @arabic_to_roman = ArabicToRoman.new
+    @roman_to_arabic = RomanToArabic.new
+  end
+
   def convert(number)
+    return arabic_to_roman.call(number) if number.kind_of?(Numeric)
+    roman_to_arabic.call(number)
+  end
+end
+
+class ArabicToRoman
+  include RomanNumerals
+
+  def call(number)
+    return execute(number) if number >= 0
+    "-#{execute(-number)}"
+  end
+
+  private
+
+  def execute(number)
+    result = ''
+    remainder = number
+
+    while(remainder > 0) do
+      roman_val = ROMAN_NUMERALS.detect do |r_v|
+        r_v[1] <= remainder
+      end
+
+      roman = roman_val[0]
+      value = roman_val[1]
+
+      result << roman
+      remainder -= value
+    end
+
+    result
+  end
+end
+
+class RomanToArabic
+  include RomanNumerals
+
+  def call(roman)
+    return execute(roman) unless roman.start_with?('-')
+    -execute(roman.sub('-', ''))
+  end
+
+  private
+
+  def execute(roman)
+    roman.chars.each.with_index.inject(0) do |result, char_index|
+      char = char_index[0]
+      index = char_index[1]
+
+      value = ROMAN_NUMERALS[char]
+      next_vale = ROMAN_NUMERALS[roman[index + 1]]
+
+      if(next_vale && next_vale > value)
+        result -= value
+      else
+        result += value
+      end
+    end
   end
 end
