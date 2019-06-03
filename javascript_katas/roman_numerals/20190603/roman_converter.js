f685df01fecbb380c4ddd6e1ea34cc5a03ee5a04
Victor Martins <victor.martins@sage.com>
2019-06-03 17:22:47 +0100

    done

--- javascript_katas/roman_numerals/20190603/roman_converter.js
+++ javascript_katas/roman_numerals/20190603/roman_converter.js
@@ -16,7 +16,79 @@
 
 class RomanConverter {
   static convert(value) {
-    throw('Not Implemented');
+    if(typeof value === 'number') {
+      return(new ArabicToRoman().convert(value))
+    } else {
+      return(new RomanToArabic().convert(value))
+    }
+  }
+}
+
+class ArabicToRoman{
+  convert(number) {
+    if(number >= 0) {
+      return this.execute(number)
+    } else {
+      return `-${this.execute(-number)}`
+    }
+  }
+  execute(number)  {
+    let result = ''
+    let remainder = number
+
+    while (remainder > 0) {
+      const roman_value = Object.entries(ROMAN_NUMERALS).find((r_v) =>{
+        return(r_v[1] <= remainder)
+      })
+
+      const roman = roman_value[0]
+      const value = roman_value[1]
+
+      result = result.concat(roman)
+      remainder -= value
+    }
+
+    return result
+  }
+}
+
+class RomanToArabic{
+  convert(roman) {
+    if(!roman.startsWith('-')){
+      return(this.execute(roman))
+    }else{
+      return(-this.execute(roman.replace('-', '')))
+    }
+  }
+
+  execute(roman){
+    const result = Array.from(roman).map((char, index) => {
+      const value = ROMAN_NUMERALS[char]
+      const next_value = ROMAN_NUMERALS[roman[index + 1]]
+      let subtract = false
+
+      // console.log('R', roman, 'V: ', value, 'NV: ', next_value);
+
+      if(next_value !== undefined && next_value > value) {
+        subtract = true
+      }
+
+      return([value, subtract])
+    }).reduce((result, value_subtract) => {
+      const value = value_subtract[0]
+      const subtract = value_subtract[1]
+
+      if(subtract){
+        return(result -= value)
+      } else {
+        return(result += value)
+      }
+    }, 0)
+
+    // console.log('RESULT: ', result);
+
+    return(result)
+
   }
 }
 
--- javascript_katas/roman_numerals/20190603/roman_converter.test.js
+++ javascript_katas/roman_numerals/20190603/roman_converter.test.js
@@ -2,174 +2,173 @@ const RomanConverter = require('./roman_converter')
 
 describe('RomanNumeralsConverter', () => {
   describe('converting arabic to roman', () => {
-    test('conversion', () => {
+    it('conversion', () => {
       expect(RomanConverter.convert(1)).toEqual('I')
     });
 
-    test('conversion', () => {
+    it('conversion', () => {
       expect(RomanConverter.convert(3)).toEqual('III')
     });
 
-    test('conversion', () => {
+    it('conversion', () => {
       expect(RomanConverter.convert(4)).toEqual('IV')
     });
 
-    test('conversion', () => {
+    it('conversion', () => {
       expect(RomanConverter.convert(5)).toEqual('V')
     });
 
-    test('conversion', () => {
+    it('conversion', () => {
       expect(RomanConverter.convert(6)).toEqual('VI')
     });
 
-    test('conversion', () => {
+    it('conversion', () => {
       expect(RomanConverter.convert(8)).toEqual('VIII')
     });
 
-    test('conversion', () => {
+    it('conversion', () => {
       expect(RomanConverter.convert(9)).toEqual('IX')
     });
 
-    test('conversion', () => {
+    it('conversion', () => {
       expect(RomanConverter.convert(10)).toEqual('X')
     });
 
-    test('conversion', () => {
+    it('conversion', () => {
       expect(RomanConverter.convert(20)).toEqual('XX')
     });
 
-    test('conversion', () => {
+    it('conversion', () => {
       expect(RomanConverter.convert(40)).toEqual('XL')
     });
 
-    test('conversion', () => {
+    it('conversion', () => {
       expect(RomanConverter.convert(50)).toEqual('L')
     });
 
-    test('conversion', () => {
+    it('conversion', () => {
       expect(RomanConverter.convert(90)).toEqual('XC')
     });
 
-    test('conversion', () => {
+    it('conversion', () => {
       expect(RomanConverter.convert(100)).toEqual('C')
     });
 
-    test('conversion', () => {
+    it('conversion', () => {
       expect(RomanConverter.convert(400)).toEqual('CD')
     });
 
-    test('conversion', () => {
+    it('conversion', () => {
       expect(RomanConverter.convert(500)).toEqual('D')
     });
 
-    test('conversion', () => {
+    it('conversion', () => {
       expect(RomanConverter.convert(900)).toEqual('CM')
     });
 
-    test('conversion', () => {
+    it('conversion', () => {
       expect(RomanConverter.convert(1000)).toEqual('M')
     });
 
-    test('conversion', () => {
+    it('conversion', () => {
       expect(RomanConverter.convert(2013)).toEqual('MMXIII')
     });
 
-    test('conversion', () => {
+    it('conversion', () => {
       expect(RomanConverter.convert(3999)).toEqual('MMMCMXCIX')
     });
 
-    test('conversion', () => {
+    it('conversion', () => {
       expect(RomanConverter.convert(0)).toEqual('')
     });
 
-    test('conversion', () => {
+    it('conversion', () => {
       expect(RomanConverter.convert(-1)).toEqual('-I')
     });
-
   });
 
   describe('converting roman to arabic', () => {
-    test('conversion', () => {
+    it('conversion', () => {
       expect(RomanConverter.convert('I')).toEqual(1);
     });
 
-    test('conversion', () => {
+    it('conversion', () => {
       expect(RomanConverter.convert('III')).toEqual(3);
     });
 
-    test('conversion', () => {
+    it('conversion', () => {
       expect(RomanConverter.convert('IV')).toEqual(4);
     });
 
-    test('conversion', () => {
+    it('conversion', () => {
       expect(RomanConverter.convert('V')).toEqual(5);
     });
 
-    test('conversion', () => {
+    it('conversion', () => {
       expect(RomanConverter.convert('VI')).toEqual(6);
     });
 
-    test('conversion', () => {
+    it('conversion', () => {
       expect(RomanConverter.convert('VIII')).toEqual(8);
     });
 
-    test('conversion', () => {
+    it('conversion', () => {
       expect(RomanConverter.convert('IX')).toEqual(9);
     });
 
-    test('conversion', () => {
+    it('conversion', () => {
       expect(RomanConverter.convert('X')).toEqual(10);
     });
 
-    test('conversion', () => {
+    it('conversion', () => {
       expect(RomanConverter.convert('XX')).toEqual(20);
     });
 
-    test('conversion', () => {
+    it('conversion', () => {
       expect(RomanConverter.convert('XL')).toEqual(40);
     });
 
-    test('conversion', () => {
+    it('conversion', () => {
       expect(RomanConverter.convert('L')).toEqual(50);
     });
 
-    test('conversion', () => {
+    it('conversion', () => {
       expect(RomanConverter.convert('XC')).toEqual(90);
     });
 
-    test('conversion', () => {
+    it('conversion', () => {
       expect(RomanConverter.convert('C')).toEqual(100);
     });
 
-    test('conversion', () => {
+    it('conversion', () => {
       expect(RomanConverter.convert('CD')).toEqual(400);
     });
 
-    test('conversion', () => {
+    it('conversion', () => {
       expect(RomanConverter.convert('D')).toEqual(500);
     });
 
-    test('conversion', () => {
+    it('conversion', () => {
       expect(RomanConverter.convert('CM')).toEqual(900);
     });
 
-    test('conversion', () => {
+    it('conversion', () => {
       expect(RomanConverter.convert('M')).toEqual(1000);
     });
 
-    test('conversion', () => {
+    it('conversion', () => {
       expect(RomanConverter.convert('MMXIII')).toEqual(2013);
     });
 
-    test('conversion', () => {
+    it('conversion', () => {
       expect(RomanConverter.convert('MMMCMXCIX')).toEqual(3999);
     });
 
-    test('conversion', () => {
+    it('conversion', () => {
       expect(RomanConverter.convert('')).toEqual(0);
     });
 
-    test('conversion', () => {
+    it('conversion', () => {
       expect(RomanConverter.convert('-I')).toEqual(-1);
     });
   });
