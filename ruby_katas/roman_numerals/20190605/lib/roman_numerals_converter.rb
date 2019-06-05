2ade90599707e4ed6478997c380c59494e188297
Victor Martins <victor.martins@sage.com>
2019-06-05 10:19:37 +0100

    done

--- ruby_katas/roman_numerals/20190605/lib/roman_numerals_converter.rb
+++ ruby_katas/roman_numerals/20190605/lib/roman_numerals_converter.rb
@@ -1,9 +1,6 @@
 require 'contracts'
 
-class RomanNumeralsConverter
-  include Contracts::Core
-  include Contracts::Builtin
-
+module RomanNumerals
   ROMAN_NUMERALS = {
     'M'  => 1000,
     'CM' => 900,
@@ -19,7 +16,84 @@ class RomanNumeralsConverter
     'IV' => 4,
     'I'  => 1
   }
+end
 
-  def convert(number)
+class RomanNumeralsConverter
+  include Contracts::Core
+  include Contracts::Builtin
+
+  def initialize
+    @roman_to_arabic = RomanToArabic.new
+    @arabic_to_roman = ArabicToRoman.new
+  end
+
+  def convert(input)
+    return arabic_to_roman.call(input) if input.kind_of?(Numeric)
+    roman_to_arabic.call(input)
+  end
+
+  private
+
+  attr_reader :roman_to_arabic, :arabic_to_roman
+end
+
+
+class RomanToArabic
+  include RomanNumerals
+
+  def call(roman)
+    return execute(roman) unless roman.start_with?('-')
+    -execute(roman.sub('-', ''))
+  end
+
+  private
+
+  def execute(roman)
+    roman.chars.each.with_index.reduce(0) do |result, char_index|
+      char = char_index[0]
+      index = char_index[1]
+
+      value = ROMAN_NUMERALS[char]
+      next_value = ROMAN_NUMERALS[roman[index + 1]]
+
+      if(next_value && next_value > value)
+        result -= value
+      else
+        result += value
+      end
+    end
+  end
+end
+
+class ArabicToRoman
+  include RomanNumerals
+
+  def call(number)
+    return execute(number) if number >= 0
+    "-#{execute(-number)}"
+  end
+
+  private
+
+  def execute(number)
+    result = ''
+    remainder = number
+
+    while(remainder > 0)
+      roman_value = ROMAN_NUMERALS.find do |r_v|
+        value = r_v[1]
+        value <= remainder
+      end
+
+      roman = roman_value[0]
+      value = roman_value[1]
+
+      remainder -= value
+
+      result << roman
+
+    end
+
+    result
   end
 end
