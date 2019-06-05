e58d2df82efa4e8cbf10e69bc1c9f5bdd6e84ae5
Victor Martins <victor.martins@sage.com>
2019-06-05 10:49:15 +0100

    done

--- javascript_katas/roman_numerals/20190605/roman_converter.js
+++ javascript_katas/roman_numerals/20190605/roman_converter.js
@@ -1,4 +1,4 @@
-  ROMAN_NUMERALS = {
+const  ROMAN_NUMERALS = {
     'M' : 1000,
     'CM': 900,
     'D' : 500,
@@ -16,7 +16,73 @@
 
 class RomanConverter {
   static convert(value) {
-    throw('Not Implemented');
+    if(typeof(value) === 'string'){
+      return new RomanToArabic().call(value)
+    }else{
+      return new ArabicToRoman().call(value)
+    }
+  }
+}
+
+class RomanToArabic {
+  call(roman) {
+    const positiveRoman = !roman.startsWith('-')
+
+    if(positiveRoman) {
+      return(this.execute(roman)      )
+    } else {
+      return(-this.execute(roman.replace('-', '')))
+    }
+  }
+
+  execute(roman){
+    return Array.from(roman)
+      .map((char, index) => [char, index])
+      .reduce((result, char_index) => {
+        const char = char_index[0]
+        const index = char_index[1]
+        const value = ROMAN_NUMERALS[char]
+        const next_value = ROMAN_NUMERALS[roman[index + 1]]
+
+        if(next_value && next_value > value) {
+          result -= value
+        } else {
+          result += value
+        }
+
+        return(result)
+      }, 0)
+  }
+}
+
+class ArabicToRoman {
+  call(number){
+    if(number >= 0){
+      return this.execute(number);
+    } else {
+      return `-${this.execute(-number)}`
+    }
+  }
+
+  execute(number){
+    let result = '';
+    let remainder = number;
+
+    while (remainder > 0) {
+      const roman_value = Object.entries(ROMAN_NUMERALS).find((r_v) => {
+        const [_roman, value] = r_v;
+        return(value <= remainder)
+      })
+
+      const [roman, value] = roman_value;
+
+      remainder -= value;
+
+      result = result.concat(roman)
+    }
+
+
+    return(result )
   }
 }
 
