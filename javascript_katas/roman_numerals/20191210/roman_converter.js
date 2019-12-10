cfaf02b2314e1a411ae2922c845a5ae93c51fd0b
Victor Martins <victor.martins@sage.com>
2019-12-10 10:24:37 +0000

    done

--- javascript_katas/roman_numerals/20191210/roman_converter.js
+++ javascript_katas/roman_numerals/20191210/roman_converter.js
@@ -16,7 +16,65 @@ class RomanConverter {
   }
 
   static convert(value) {
-    throw('Not Implemented');
+    const romanToArabic = new RomanToArabic(this.ROMAN_NUMERALS)
+    const arabicToRoman = new ArabicToRoman(this.ROMAN_NUMERALS)
+
+    if(typeof(value) == 'string') return romanToArabic.call(value)
+    return arabicToRoman.call(value)
+  }
+}
+
+class ArabicToRoman {
+  constructor(romanNumerals){
+    this.romanNumerals = romanNumerals
+  }
+
+  call(arabic){
+    if(arabic >= 0) return this.execute(arabic)
+    return `-${this.execute(-arabic)}`
+  }
+
+  execute(arabic){
+    let result = ''
+    let remainder = arabic
+
+    while(remainder > 0){
+      const [roman, value] = Object.entries(this.romanNumerals).find( r_v => r_v[1] <= remainder)
+
+      result = result.concat(roman)
+      remainder -= value
+    }
+
+    return result
+  }
+}
+
+class RomanToArabic {
+  constructor(romanNumerals){
+    this.romanNumerals = romanNumerals
+  }
+
+  call(roman){
+    if(!roman.startsWith('-')) return this.execute(roman)
+    return -this.execute(roman.replace('-', ''))
+  }
+
+  execute(roman){
+    return Array.from(roman)
+      .map((char, index) => [char, index])
+      .reduce((result, char_index) => {
+        const [char, index] = char_index
+        const value = this.romanNumerals[char]
+        const nextValue = this.romanNumerals[roman[index+1]]
+
+        if(nextValue && nextValue > value) {
+          result -= value
+        } else {
+          result += value
+        }
+
+        return result
+      }, 0)
   }
 }
 
