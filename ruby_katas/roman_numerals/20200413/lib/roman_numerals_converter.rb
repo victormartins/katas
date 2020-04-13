4958f3b935c808fac7c259e130b104c00c5f7c4d
Victor Guiomar Martins <correio@victormartins.com>
2020-04-13 11:47:07 +0100

    done

--- ruby_katas/roman_numerals/20200413/lib/roman_numerals_converter.rb
+++ ruby_katas/roman_numerals/20200413/lib/roman_numerals_converter.rb
@@ -1,25 +1,106 @@
+# frozen_string_literal: true
+
 require 'contracts'
 
+# Convert input to either roman or arabic
 class RomanNumeralsConverter
   include Contracts::Core
   include Contracts::Builtin
 
   ROMAN_NUMERALS = {
-    'M'  => 1000,
+    'M' => 1000,
     'CM' => 900,
-    'D'  => 500,
+    'D' => 500,
     'CD' => 400,
-    'C'  => 100,
+    'C' => 100,
     'XC' => 90,
-    'L'  => 50,
+    'L' => 50,
     'XL' => 40,
-    'X'  => 10,
+    'X' => 10,
     'IX' => 9,
-    'V'  => 5,
+    'V' => 5,
     'IV' => 4,
-    'I'  => 1
-  }
+    'I' => 1
+  }.freeze
+
+  def initialize
+    @arabic_to_roman = ArabicToRoman.new(ROMAN_NUMERALS)
+    @roman_to_arabic = RomanToArabic.new(ROMAN_NUMERALS)
+  end
 
   def convert(number)
+    return arabic_to_roman.call(number) if number.is_a?(Numeric)
+
+    roman_to_arabic.call(number)
+  end
+
+  private
+
+  attr_reader :arabic_to_roman, :roman_to_arabic
+end
+
+# Converts arabic to roman
+class ArabicToRoman
+  def initialize(roman_numerals)
+    @roman_numerals = roman_numerals
+  end
+
+  def call(arabic)
+    return execute(arabic) if arabic >= 0
+
+    "-#{execute(-arabic)}"
+  end
+
+  private
+
+  def execute(arabic)
+    result = ''
+    remainder = arabic
+
+    while remainder > 0
+      roman, value = roman_value(remainder)
+      result += roman
+      remainder -= value
+    end
+
+    result
+  end
+
+  def roman_value(remainder)
+    roman_value = @roman_numerals.find { |r_v| r_v[1] <= remainder }
+    roman = roman_value[0]
+    value = roman_value[1]
+    [roman, value]
+  end
+end
+
+class RomanToArabic
+  def initialize(roman_numerals)
+    @roman_numerals = roman_numerals
+  end
+
+  def call(roman)
+    return execute(roman) unless roman.start_with?('-')
+
+    -execute(roman.sub('-', ''))
+  end
+
+  private
+
+  def execute(roman)
+    roman.chars.each.with_index.reduce(0) do |result, char_index|
+      char = char_index[0]
+      index = char_index[1]
+      value = @roman_numerals[char]
+      next_value = @roman_numerals[roman[index + 1]]
+
+      if next_value && next_value > value
+        result -= value
+      else
+        result += value
+      end
+
+      result
+    end
   end
 end
