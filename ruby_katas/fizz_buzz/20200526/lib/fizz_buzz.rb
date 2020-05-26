a15da37a6d73b02b2cf8ad9edd40af9257fbed36
Victor Guiomar Martins <correio@victormartins.com>
2020-05-26 14:53:22 +0100

    Done with Ergodox Ez

--- ruby_katas/fizz_buzz/20200526/lib/fizz_buzz.rb
+++ ruby_katas/fizz_buzz/20200526/lib/fizz_buzz.rb
@@ -1,4 +1,26 @@
 class FizzBuzz
+  FIZZ = 'Fizz'
+  BUZZ = 'Buzz'
+  FIZZ_DIGIT = 3
+  BUZZ_DIGIT = 5
+
+  def initialize(check_digit: false)
+    @check_digit = check_digit
+  end
+
   def call(input)
+    return execute(input) if input.is_a?(Numeric)
+
+    input.map { |i| execute(i) }
+  end
+
+  private
+
+  def execute(input)
+    return FIZZ + BUZZ if input % FIZZ_DIGIT == 0 && input % BUZZ_DIGIT == 0
+    return FIZZ if input % FIZZ_DIGIT == 0 || @check_digit && input.to_s.include?(FIZZ_DIGIT.to_s)
+    return BUZZ if input % BUZZ_DIGIT == 0 || @check_digit && input.to_s.include?(BUZZ_DIGIT.to_s)
+
+    input
   end
 end
--- ruby_katas/fizz_buzz/20200526/spec/fizz_buzz_spec.rb
+++ ruby_katas/fizz_buzz/20200526/spec/fizz_buzz_spec.rb
@@ -3,10 +3,12 @@ require_relative './../lib/fizz_buzz'
 RSpec.describe FizzBuzz do
   it 'returns "Fizz" if number is divisible by 3' do
     expect(subject.call(3)).to eq('Fizz')
+    expect(subject.call(9)).to eq('Fizz')
   end
 
   it 'returns "Buzz" if number is divisible by 5' do
     expect(subject.call(5)).to eq('Buzz')
+    expect(subject.call(10)).to eq('Buzz')
   end
 
   it 'returns "FizzBuzz" if the number is divisible by 3 and 5' do
