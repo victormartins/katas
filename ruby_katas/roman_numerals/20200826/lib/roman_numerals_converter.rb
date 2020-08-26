ffd4d96edaae02112f3ae6cd295c93d59c0ab1ec
Victor Guiomar Martins <correio@victormartins.com>
2020-08-26 13:47:59 +0100

    Done

--- ruby_katas/roman_numerals/20200826/lib/roman_numerals_converter.rb
+++ ruby_katas/roman_numerals/20200826/lib/roman_numerals_converter.rb
@@ -20,6 +20,80 @@ class RomanNumeralsConverter
     'I'  => 1
   }
 
+
+  def initialize
+    @arabic_to_roman = ArabicToRoman.new(ROMAN_NUMERALS)
+    @roman_to_arabic = RomanToArabic.new(ROMAN_NUMERALS)
+  end
+
   def convert(number)
+    return arabic_to_roman.call(number) if number.is_a?(Numeric)
+    roman_to_arabic.call(number)
+  end
+
+  private
+
+  attr_reader :arabic_to_roman, :roman_to_arabic
+end
+
+class ArabicToRoman
+  def initialize(roman_numerals)
+    @roman_numerals = roman_numerals
+  end
+
+  def call(arabic)
+    return execute(arabic) unless arabic.negative?
+    "-#{execute(-arabic)}"
+  end
+
+  private
+
+  def execute(arabic)
+    result = ''
+    remainder = arabic
+
+    while(remainder > 0) do
+      roman_value = @roman_numerals.find { |r_v| r_v[1] <= remainder }
+      roman = roman_value[0]
+      value = roman_value[1]
+
+      result << roman
+      remainder -= value
+    end
+
+    result
+  end
+end
+
+# Convert from Roman to Arabic
+class RomanToArabic
+  def initialize(roman_numerals)
+    @roman_numerals = roman_numerals
+  end
+
+  def call(roman)
+    return execute(roman) unless roman.start_with?('-')
+
+    -execute(roman.sub('-', ''))
+  end
+
+  private
+
+  def execute(roman)
+    roman.chars.each.with_index.reduce(0) do |result, char_index|
+      char = char_index[0]
+      index = char_index[1]
+
+      value = @roman_numerals[char]
+      next_value = @roman_numerals[roman[index + 1]]
+
+      if(next_value && next_value > value)
+        result -= value
+      else
+        result += value
+      end
+
+      result
+    end
   end
 end
