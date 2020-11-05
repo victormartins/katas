e364ce738dc5fd026ef0574831c27a81a0e6b966
Victor Guiomar Martins <correio@victormartins.com>
2020-11-05 22:09:53 +0000

    safe start

--- ruby_katas/refactoring_exercises/10_primitive_obcession/starting_point/lib/grade.rb
+++ ruby_katas/refactoring_exercises/10_primitive_obcession/starting_point/lib/grade.rb
@@ -1,7 +1,23 @@
 # frozen_string_literal: true
 
 class Grade
-  def initialize(grade_letter)
-    @grade_letter = grade_letter
+  def initialize(grade)
+    @grade = grade
   end
+
+  def greater_than?(grade)
+
+  end
+
+  def lesser_than?(grade)
+
+  end
+
+  def == (other)
+    true
+  end
+
+  private
+
+  attr_reader :grade_letter
 end
