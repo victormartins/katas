b98997979c8d247f1810801286a6a939c42eb5b8
Victor Guiomar Martins <correio@victormartins.com>
2020-11-21 22:29:04 +0000

    Added X-Request-Id tracing
    Used a similar way has Rails does in the middleware:
    ActionDispatch:RequestID
    
    Sets the Internal X-Request-ID here that is then used in controllers etc.
    "action_dispatch.request_id"
    https://github.com/rails/rails/blob/fe76a95b0d252a2d7c25e69498b720c96b243ea2/actionpack/lib/action_dispatch/middleware/request_id.rb#L26
    https://github.com/rails/rails/blob/371b4800266cbf1c659a9068f58412cb7a88e244/actionpack/lib/action_dispatch/http/request.rb#L314
    
    Notice how it checks if the header exists by calling:
    request.x_request_id
    This method is generated here:
    https://github.com/rails/rails/blob/371b4800266cbf1c659a9068f58412cb7a88e244/actionpack/lib/action_dispatch/http/request.rb#L53
    Used here:
    https://github.com/rails/rails/blob/371b4800266cbf1c659a9068f58412cb7a88e244/actionpack/lib/action_dispatch/http/response.rb#L193
    https://github.com/rails/rails/blob/371b4800266cbf1c659a9068f58412cb7a88e244/actionpack/lib/action_dispatch/http/request.rb#L213
    To then just use the method get_header from the request
    https://github.com/rails/rails/blob/371b4800266cbf1c659a9068f58412cb7a88e244/actionpack/lib/action_dispatch/http/request.rb#L213
    
    Sets the response X-Request-Id here:
    https://github.com/rails/rails/blob/fe76a95b0d252a2d7c25e69498b720c96b243ea2/actionpack/lib/action_dispatch/middleware/request_id.rb#L27

--- docker_katas/03_rack_with_middleware/src/config.ru
+++ docker_katas/03_rack_with_middleware/src/config.ru
@@ -3,10 +3,14 @@
 require 'rack'
 require './lib/main'
 require './lib/middleware/request_tracer'
+require './lib/middleware/request_timer'
+require './lib/middleware/request_caching'
 
 # Make sure we can see the ruby logs in docker
 $stdout.sync = true
 
 # use Rack::CommonLogger, MyApp.logger
-use MyApp::RequestTracer, logger: MyApp.logger
+use MyApp::Middleware::RequestTracer, logger: MyApp.logger
+use MyApp::Middleware::RequestTimer, logger: MyApp.logger
+use MyApp::Middleware::RequestCaching, logger: MyApp.logger
 run MyApp::Main.new
--- docker_katas/03_rack_with_middleware/src/lib/middleware/request_tracer.rb
+++ docker_katas/03_rack_with_middleware/src/lib/middleware/request_tracer.rb
@@ -1,25 +1,79 @@
+# frozen_string_litera: true
+
+require 'securerandom'
+
 module MyApp
-  class RequestTracer
-    require 'logger'
+  module Middleware
+    # Adds headers to the request to help tracing.
+    class RequestTracer
+      # Header in is normal format
+      X_REQUEST_ID = 'X-Request-Id'
+      # Header in the env format
+      HTTP_X_REQUEST_ID = 'HTTP_X_REQUEST_ID'
+      # Header to be used internally by the web application
+      INTERNAL_REQUEST_ID = "request.tracer.request_id"
 
-    # Initialize in config.ru like so:
-    # use MyApp::RequestTracer, logger: MyApp.logger
-    def initialize(app, logger:)
-      @app = app
-      @logger = logger
+      # Initialize in config.ru like so:
+      # use MyApp::RequestTracer, logger: MyApp.logger
+      def initialize(app, logger:)
+        @app = app
+        @logger = logger
+      end
+
+      def call(env)
+        logger.debug { "[#{self.class}] - Before calling the app." }
+
+        logger.debug do
+          "[#{self.class}] - env is just an hash that the webserver produced:" \
+            "env.class #{env.class} "\
+            "env.keys [#{env.keys.first(4).join(', ')}...] so we can add our request id to it."
+        end
+
+        # We add our generated request id into an INTERNAL_REQUEST_ID so that
+        # the application can use it if needed.
+        # Bellow we also add it to the response headers so that if we redirect
+        # the request the receivers can use it. It will also be returned to the
+        # caller so that he can make requests idempotent.
+        request_id = fetch_or_generate_request_id(env)
+        env[INTERNAL_REQUEST_ID] = request_id
+
+        # Now we call the rest of the middleware stack which eventually calls the application.
+        # Using .tap we add the request_id to the responde headers.
+        status, headers, body = @app.call(env).tap do |_status, headers, _body|
+          logger.debug do
+            "[#{self.class}] - Setting the request id in the response headers, so that" \
+              "it can be accessible by any caller or redirection to other services."
+          end
+          insert_request_id_to_response(headers, request_id)
+        end
+
+        logger.debug { "[#{self.class}] - After calling the app." }
+
+        [status, headers, body]
+      end
+
+      private
+
+      attr_reader :logger
+
+      def insert_request_id_to_response(headers, request_id)
+        headers[X_REQUEST_ID] = request_id
+      end
+
+      def fetch_or_generate_request_id(env)
+        request = Rack::Request.new(env)
+
+        request_id = request.get_header(HTTP_X_REQUEST_ID)
+
+        if(request_id)
+          logger.debug { "[#{self.class}] - #{X_REQUEST_ID} found: [#{request_id}]" }
+          request_id
+        else
+          request_id = SecureRandom.uuid
+          logger.debug { "[#{self.class}] - #{X_REQUEST_ID} not found. Generated: #{request_id} " }
+          request_id
+        end
+      end
     end
-
-    def call(env)
-      status, headers, body = @app.call(env)
-
-      logger.debug { "[#{self.class}] - Setting Request Tracing..." }
-
-      [status, headers, body]
-    end
-
-    private
-
-    attr_reader :logger
-
   end
 end
