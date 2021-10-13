78037709f17f1a75649319452db3d19c8c03298c
Victor Martins <victormartins@azafinance.com>
2021-10-13 21:40:37 +0100

    Improve kata

--- ruby_katas/roman_numerals/20211012/spec/roman_numerals_converter_spec.rb
+++ ruby_katas/roman_numerals/20211012/spec/roman_numerals_converter_spec.rb
@@ -1,30 +1,6 @@
 require_relative './../lib/roman_numerals_converter'
 
 RSpec.describe RomanNumeralsConverter do
-  context 'converting arabic to roman' do
-    it { expect(subject.convert(1)).to eq('I') }
-    it { expect(subject.convert(3)).to eq('III') }
-    it { expect(subject.convert(4)).to eq('IV') }
-    it { expect(subject.convert(5)).to eq('V') }
-    it { expect(subject.convert(6)).to eq('VI') }
-    it { expect(subject.convert(8)).to eq('VIII') }
-    it { expect(subject.convert(9)).to eq('IX') }
-    it { expect(subject.convert(10)).to eq('X') }
-    it { expect(subject.convert(20)).to eq('XX') }
-    it { expect(subject.convert(40)).to eq('XL') }
-    it { expect(subject.convert(50)).to eq('L') }
-    it { expect(subject.convert(90)).to eq('XC') }
-    it { expect(subject.convert(100)).to eq('C') }
-    it { expect(subject.convert(400)).to eq('CD') }
-    it { expect(subject.convert(500)).to eq('D') }
-    it { expect(subject.convert(900)).to eq('CM') }
-    it { expect(subject.convert(1000)).to eq('M') }
-    it { expect(subject.convert(2013)).to eq('MMXIII') }
-    it { expect(subject.convert(3999)).to eq('MMMCMXCIX') }
-    it { expect(subject.convert(0)).to eq('') }
-    it { expect(subject.convert(-1)).to eq('-I') }
-  end
-
   context 'converting roman to arabic' do
     it { expect(subject.convert('I')).to eq(1) }
     it { expect(subject.convert('III')).to eq(3) }
@@ -47,6 +23,30 @@ RSpec.describe RomanNumeralsConverter do
     it { expect(subject.convert('MMMCMXCIX')).to eq(3999) }
     it { expect(subject.convert('')).to eq(0) }
     it { expect(subject.convert('-I')).to eq(-1) }
+  end
+
+  context 'converting arabic to roman' do
+    it { expect(subject.convert(1)).to eq('I') }
+    it { expect(subject.convert(3)).to eq('III') }
+    it { expect(subject.convert(4)).to eq('IV') }
+    it { expect(subject.convert(5)).to eq('V') }
+    it { expect(subject.convert(6)).to eq('VI') }
+    it { expect(subject.convert(8)).to eq('VIII') }
+    it { expect(subject.convert(9)).to eq('IX') }
+    it { expect(subject.convert(10)).to eq('X') }
+    it { expect(subject.convert(20)).to eq('XX') }
+    it { expect(subject.convert(40)).to eq('XL') }
+    it { expect(subject.convert(50)).to eq('L') }
+    it { expect(subject.convert(90)).to eq('XC') }
+    it { expect(subject.convert(100)).to eq('C') }
+    it { expect(subject.convert(400)).to eq('CD') }
+    it { expect(subject.convert(500)).to eq('D') }
+    it { expect(subject.convert(900)).to eq('CM') }
+    it { expect(subject.convert(1000)).to eq('M') }
+    it { expect(subject.convert(2013)).to eq('MMXIII') }
+    it { expect(subject.convert(3999)).to eq('MMMCMXCIX') }
+    it { expect(subject.convert(0)).to eq('') }
+    it { expect(subject.convert(-1)).to eq('-I') }
   end
 
   context 'validating input' do
