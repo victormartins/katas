17d77c0bf907e4790ab12ec579af6e680e299618
Victor Martins <email@victormartins.com>
2022-05-24 16:26:59 +0100

    Done

--- ruby_katas/roman_numerals/20220524/lib/roman_numerals_converter.rb
+++ ruby_katas/roman_numerals/20220524/lib/roman_numerals_converter.rb
@@ -17,7 +17,88 @@ class RomanNumeralsConverter
     'I'  => 1
   }.freeze
 
-  def convert(_number)
-    raise NotImplementedError, 'Please start here.'
+  def initialize
+    @roman_to_arabic = RomanToArabic.new(ROMAN_NUMERALS)
+    @arabic_to_roman = ArabicToRoman.new(ROMAN_NUMERALS)
+  end
+
+  def convert(input)
+    return input.map { |n| convert_number(n) } if input.is_a?(Array)
+
+    convert_number(input)
+  end
+
+  private
+
+  def convert_number(number)
+    raise("Invalid Input! #{number}") unless number.is_a?(String) || number.is_a?(Integer)
+
+    return roman_to_arabic.call(number) if number.is_a?(String)
+
+    arabic_to_roman.call(number)
+  end
+
+  attr_reader :roman_to_arabic, :arabic_to_roman
+end
+
+class RomanNumeralsConverter
+  class RomanToArabic
+    def initialize(roman_numerals)
+      @roman_numerals = roman_numerals
+    end
+
+    def call(roman)
+      return convert_roman(roman) unless roman.start_with?('-')
+
+      -convert_roman(roman.sub('-', ''))
+    end
+
+    private
+
+    def convert_roman(roman)
+      roman.chars.each.with_index.reduce(0) do |result, char_index|
+        char, index = char_index
+        next_char = roman[index + 1]
+        next_value = @roman_numerals[next_char]
+        value = @roman_numerals[char]
+
+        if next_value && next_value > value 
+          result -= value
+        else
+          result += value
+        end
+
+        result
+      end
+    end
+  end
+end
+
+class RomanNumeralsConverter
+  class ArabicToRoman
+    def initialize(roman_numerals)
+      @roman_numerals = roman_numerals
+    end
+
+    def call(arabic)
+      return convert_arabic(arabic) unless arabic.negative?
+
+      "-#{convert_arabic(-arabic)}"
+    end
+
+    private
+
+    def convert_arabic(arabic)
+      result = ''
+      remainder = arabic
+
+      while(remainder.positive?)
+        roman, value = @roman_numerals.find { |_roman, value| value <= remainder }
+        result += roman
+        remainder -= value
+      end
+
+      result
+    end
   end
 end
